a05771521751c08b466442a92cd9099561295c92
Braian Burini <braianb55@hotmail.com>
2022-08-17 13:34:17 -0300

    limpiando instalación de create-react-app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de ecommerce hecho con React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://burinib.github.io/ecommerce-react/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link al deploy
         </a>
       </header>
     </div>
