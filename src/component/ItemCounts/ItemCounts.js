c5b55c3c440266e36eaa9aec684c29617ac391e5
Braian Burini <braianb55@hotmail.com>
2022-09-01 18:37:43 -0300

    correccion del contador

--- src/component/ItemCounts/ItemCounts.js
+++ src/component/ItemCounts/ItemCounts.js
@@ -1,36 +1,30 @@
-import './itemCounts.css'
+import "./itemCounts.css";
 
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 export const ItemCounts = () => {
-  const [counter, setCounter] = useState(1)
+  const [counter, setCounter] = useState(1);
   const increment = () => {
-    if (counter <5){
-      setCounter(counter+1)
-    } 
-  }
+    if (counter < 5) {
+      setCounter(counter + 1);
+    }
+  };
   const decrement = () => {
-    if (counter >1){
-      setCounter(counter-1)
-    } 
-  }
-
-  
-
+    if (counter > 1) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
-    <div className='counter'>
-    <button onClick={() => decrement()}>-</button>
-    <span>{counter}</span>
-    <button onClick={() => increment()}>+</button>
-    <div>
-      <button>Agregar al carrito</button>
-    </div>
+    <div className="counter">
+      <button onClick={() => decrement()}>-</button>
+      <span>{counter}</span>
+      <button onClick={() => increment()}>+</button>
+      <div>
+        <button>Agregar al carrito</button>
       </div>
+    </div>
   );
-}
-
-
+};
 
 export default ItemCounts;
