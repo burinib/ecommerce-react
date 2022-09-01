fb45f4a585692fc5911729b2afd1977994348c92
Braian Burini <braianb55@hotmail.com>
2022-09-01 18:38:03 -0300

    creacion de data

---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+import buzo1 from "../assets/img/abrigo/buzo1.jpg";
+import buzo2 from "../assets/img/abrigo/buzo2.jpg";
+import buzo3 from "../assets/img/abrigo/buzo3.jpg";
+import buzo4 from "../assets/img/abrigo/buzo4.jpg";
+import buzo5 from "../assets/img/abrigo/buzo5.jpg";
+
+export const data = [
+    {
+      "nombre": "Canguro ByN",
+      "precio": 3490,
+      "img": buzo1,
+      "id": "0"
+    },
+    {
+      "nombre": "Sweatter rojo",
+      "precio": 3590,
+      "img": buzo2,
+      "id": "1"
+    },
+    {
+      "nombre": "Sweatter blanco",
+      "precio": 3490,
+      "img": buzo3,
+      "id": "2"
+    },
+    {
+      "nombre": "Canguro liso negro",
+      "precio": 2590,
+      "img": buzo4,
+      "id": "3"
+    },
+    {
+      "nombre": "Sweatter gris polera",
+      "precio": 3490,
+      "img": buzo5,
+      "id": "4"
+    },
+  ]
+  
