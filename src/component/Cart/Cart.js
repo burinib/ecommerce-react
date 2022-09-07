ccaa4e032e20dff3e28b75d14a95f13b98efaae9
Braian Burini <braianb55@hotmail.com>
2022-09-07 19:55:08 -0300

    creacion de componente Cart

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./cart.css";
+
+export default function Cart() {
+  return (
+    <div>
+        <h2 className='cart__container'>Cart</h2>
+    </div>
+  )
+}
+
