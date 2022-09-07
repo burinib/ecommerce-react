66f28b4b098c2e232cd9201e90520fea7432ec4e
Braian Burini <braianb55@hotmail.com>
2022-09-07 19:56:04 -0300

    agrego la ruta /cart en el router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import "./App.css";
 import ItemListContainer from "./component/ItemListContainer/ItemListContainer";
 import NavBar from "./component/NavBar";
-import ItemCounts from "./component/ItemCounts/ItemCounts";
-import ItemList from "./component/ItemList/ItemList";
 import ItemDetailContainer from "./component/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./component/Cart/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ItemListContainer>
-              <ItemList />
-            </ItemListContainer>
-          }
-        />
+        <Route path="/" element={<ItemListContainer />} />
         <Route path="/detail/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
