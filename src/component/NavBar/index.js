aa6561ea1758b7575161853ef81387a900a8da5f
Braian Burini <braianb55@hotmail.com>
2022-12-01 14:55:01 -0300

    mqrs

--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -30,7 +30,7 @@ export default function NavBar() {
         <ul className="header__options">
           <li>Envios</li>
           <Link to="/form">
-            <li>Contacto</li>
+            <li>Contacto</li>np
           </Link>
           <Link to="./Cart">
             <li>
